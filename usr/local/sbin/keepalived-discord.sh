daaa1bf0c0fd6ab0a895293c289087d800bb834d
Xopez <28950736+xopez@users.noreply.github.com>
2026-09-12 09:05:05 +0200

    Enhance Discord webhook message with additional fields

--- usr/local/sbin/keepalived-discord.sh
+++ usr/local/sbin/keepalived-discord.sh
@@ -3,14 +3,35 @@
 WEBHOOK_URL="https://discord.com/api/webhooks/ID/TOKEN"
 
 STATE="$1"
+
 HOST="$(hostname)"
+INSTANCE="PIHOLE"
+
+PRIORITY="$(grep -A20 "vrrp_instance ${INSTANCE}" /etc/keepalived/keepalived.conf \
+    | grep -m1 "priority" \
+    | awk '{print $2}')"
+
+IPV4="$(ip -4 addr show dev eth0 scope global \
+    | awk '/inet / {print $2}' \
+    | head -n1)"
+
+IPV6="$(ip -6 addr show dev eth0 scope global \
+    | awk '/inet6 / && $2 !~ /^fe80:/ {print $2}' \
+    | head -n1)"
+
+VIPV4="10.5.5.2/24"
+VIPV6="fd00:5::2/64"
+
+TIMESTAMP="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 
 case "$STATE" in
     MASTER)
+        TITLE="🟢 Keepalived: MASTER"
         TEXT="${HOST} is now MASTER"
         COLOR=3066993
         ;;
     BACKUP)
+        TITLE="🟡 Keepalived: BACKUP"
         TEXT="${HOST} is now BACKUP"
         COLOR=16776960
         ;;
@@ -23,8 +44,49 @@ curl -sS -X POST "$WEBHOOK_URL" \
     -H "Content-Type: application/json" \
     -d "{
         \"embeds\": [{
-            \"title\": \"Keepalived State Change\",
+            \"title\": \"${TITLE}\",
             \"description\": \"${TEXT}\",
-            \"color\": ${COLOR}
+            \"color\": ${COLOR},
+            \"fields\": [
+                {
+                    \"name\": \"Host\",
+                    \"value\": \"\`${HOST}\`\",
+                    \"inline\": true
+                },
+                {
+                    \"name\": \"VRRP Instance\",
+                    \"value\": \"\`${INSTANCE}\`\",
+                    \"inline\": true
+                },
+                {
+                    \"name\": \"Priority\",
+                    \"value\": \"\`${PRIORITY}\`\",
+                    \"inline\": true
+                },
+                {
+                    \"name\": \"IPv4 VIP\",
+                    \"value\": \"\`${VIPV4}\`\",
+                    \"inline\": true
+                },
+                {
+                    \"name\": \"IPv6 VIP\",
+                    \"value\": \"\`${VIPV6}\`\",
+                    \"inline\": true
+                },
+                {
+                    \"name\": \"IPv4 Address\",
+                    \"value\": \"\`${IPV4:-unknown}\`\",
+                    \"inline\": true
+                },
+                {
+                    \"name\": \"IPv6 Address\",
+                    \"value\": \"\`${IPV6:-unknown}\`\",
+                    \"inline\": true
+                }
+            ],
+            \"footer\": {
+                \"text\": \"Keepalived / Pi-hole HA\"
+            },
+            \"timestamp\": \"${TIMESTAMP}\"
         }]
     }"
